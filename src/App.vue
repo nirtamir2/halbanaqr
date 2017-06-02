a76fb7a4c27b78af8f5f76f8ea0c404920ff1902
Nir Tamir <nirtamir2@gmail.com>
2017-06-02 13:48:11 +0300

    first commit vuejs and vuetify

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <v-app>
+      <header>
+        <template>
+          <v-toolbar class="green" light>
+            <v-toolbar-side-icon light></v-toolbar-side-icon>
+            <v-toolbar-title class="hidden-sm-and-down">Toolbar</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon light>
+              <v-icon>account_circle</v-icon>
+            </v-btn>
+            <v-btn icon light>
+              <v-icon>favorite</v-icon>
+            </v-btn>
+            <v-btn icon light>
+              <v-icon>more_vert</v-icon>
+            </v-btn>
+          </v-toolbar>
+        </template>
+      </header>
+      <main>
+        <router-view></router-view>
+      </main>
+    </v-app>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style>
+</style>
