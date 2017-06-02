527c9f9a284f293d4ac582c1150b0fbdd83c1d63
Nir Tamir <nirtamir2@gmail.com>
2017-06-02 15:35:32 +0300

    add dropzone

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,15 +1,28 @@
 <template>
   <div>
     <h1> hello world</h1>
+    <dropzone id="myVueDropzone" url="https://httpbin.org/post" v-on:vdropzone-success="showSuccess">
+      <!-- Optional parameters if any! -->
+      <input type="hidden" name="token" value="xxx">
+    </dropzone>
   </div>
 </template>
 
 <script>
+  import Dropzone from 'vue2-dropzone'
   export default {
     name: 'hello',
     data () {
       return {
         msg: 'Welcome to Your Vue.js PWA'
+      }
+    },
+    components: {
+      Dropzone
+    },
+    methods: {
+      'showSuccess': function (file) {
+        console.log('A file was successfully uploaded')
       }
     }
   }
