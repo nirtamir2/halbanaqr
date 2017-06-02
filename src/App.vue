1ffa6c1ec64711e02a8d99873ab92cd145aace93
Nir Tamir <nirtamir2@gmail.com>
2017-06-02 15:28:51 +0300

    add iaf logo

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <v-app>
       <header>
         <template>
-          <v-toolbar class="green" light>
+          <v-toolbar class="indigo" light>
             <v-toolbar-side-icon light></v-toolbar-side-icon>
             <v-toolbar-title class="hidden-sm-and-down">Toolbar</v-toolbar-title>
             <v-spacer></v-spacer>
@@ -16,6 +16,8 @@
             <v-btn icon light>
               <v-icon>more_vert</v-icon>
             </v-btn>
+            <img src="static/img/icons/iaf-logo/iaf-logo.svg" alt="iaf-logo" height="40px" width="40px"></img>
+
           </v-toolbar>
         </template>
       </header>
